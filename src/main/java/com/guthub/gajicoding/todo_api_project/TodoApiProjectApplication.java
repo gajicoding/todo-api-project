7d4bdfb2d362cf093e62cd5ed9b9fdf3df12ff78
huouvcti <huouvcti@gamil.com>
2025-05-08 18:21:42 +0900

    chore: 프로젝트 생성

---
+++ src/main/java/com/guthub/gajicoding/todo_api_project/TodoApiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.guthub.gajicoding.todo_api_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TodoApiProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TodoApiProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guthub/gajicoding/todo_api_project/TodoApiProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guthub.gajicoding.todo_api_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoApiProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
